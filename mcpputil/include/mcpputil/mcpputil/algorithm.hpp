74ea34677f7400ed3e2e49c483c7a1fab1879d17
Gary Furnish <gfurnish@gfurnish.net>
2016-10-06 19:22:56 -0600

    Add reverse_consume_for_each

---
+++ mcpputil/include/mcpputil/mcpputil/algorithm.hpp
@@ -0,0 +1,19 @@
+
+namespace mcpputil
+{
+  /**
+   * \brief for_each algorithm starting from the back.
+   *
+   * This algorithm is safe for insertion and deletion.
+   **/
+  template <typename Container, typename F>
+  void reverse_consume_for_each(Container &&c, F &&f)
+  {
+    while (!c.empty()) {
+      const auto it = c.rbegin();
+      c.erase((it + 1).base());
+      const auto val = *it;
+      f(val);
+    }
+  }
+}
