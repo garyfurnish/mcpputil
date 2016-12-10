ff696194d1c4a2544e51a20a8fb0c4b7917dac66
Gary Furnish <gfurnish@gfurnish.net>
2016-12-10 10:44:04 -0700

    Add pointer and vector utils

---
+++ mcpputil/include/mcpputil/mcpputil/pointer_utils.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstddef>
+#include <cstdint>
+namespace mcpputil
+{
+  /**
+   * \brief Adjust pointer by byte offset.
+   *
+   * Default rounding behavior if offset not integer multiple
+   **/
+  template <typename Ptr_Underlying>
+  void adjust_pointer_by_offset(Ptr_Underlying *&ptr, ptrdiff_t offset)
+  {
+    ptr = reinterpret_cast<Ptr_Underlying *>(reinterpret_cast<uint8_t *>(ptr) + offset);
+  }
+}
