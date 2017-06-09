a9a7ab10b6f10ef398c570c7ffb96796857605b2
Gary Furnish <gfurnish@gfurnish.net>
2017-06-09 05:51:36 -0600

    Adjust compile options

--- setup.cmake
+++ setup.cmake
@@ -6,7 +6,7 @@ set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
 ELSE(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 #LINUX OR OSX
 set(CMAKE_CXX_FLAGS "-std=c++1z ${CMAKE_CXX_FLAGS}")
-add_compile_options(-Wall -Wextra -pedantic-errors -fexceptions -Wno-unused-value -Wno-invalid-offsetof -Wnonnull -Wformat=2 -Wformat-security  -Wformat-nonliteral -Winit-self -Werror -Wswitch-default -Wstrict-overflow=1 -Wcast-align  -Wcast-qual -Wdisabled-optimization -Winit-self -march=native -fno-omit-frame-pointer -Wno-unknown-pragmas )
+add_compile_options(-Wall -Wextra -pedantic-errors -fexceptions -Wno-invalid-offsetof -Wnonnull -Wformat=2 -Wformat-security  -Wformat-nonliteral -Winit-self -Werror -Wswitch-default -Wstrict-overflow=1 -Wcast-align  -Wcast-qual -Wdisabled-optimization -Winit-self -march=native -fno-omit-frame-pointer -Wno-unknown-pragmas )
 add_compile_options(--param ssp-buffer-size=4 )
 add_compile_options(-fvisibility=hidden)
 add_compile_options(-fvisibility-inlines-hidden)
@@ -21,7 +21,7 @@ ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 IF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
 IF(${CMAKE_CXX_COMPILER} MATCHES ".*clang\\+\\+")
 #LINUX CLANG
-add_compile_options(-Weverything -Wthread-safety -Wno-padded -Wno-c++98-compat-pedantic -Wno-global-constructors -Wno-exit-time-destructors -Wno-newline-eof -Wno-weak-vtables -Wno-weak-template-vtables -Wno-missing-prototypes -Wno-reserved-id-macro -fcolor-diagnostics -Wno-undefined-var-template -Wno-unused-private-field -Wno-unused-parameter)
+add_compile_options(-Weverything -Wthread-safety -Wno-padded -Wno-c++98-compat-pedantic -Wno-global-constructors -Wno-exit-time-destructors -Wno-weak-vtables -Wno-weak-template-vtables -Wno-missing-prototypes -Wno-reserved-id-macro -fcolor-diagnostics -Wno-undefined-var-template -Wno-unused-private-field )
 #LINUX GCC
 ELSE(${CMAKE_CXX_COMPILER} MATCHES ".*clang\\+\\+")
 add_compile_options(-Wno-attributes -Wno-unused-local-typedefs -Wno-unused-variable -Wno-unused-parameter -Wlogical-op -Wnoexcept -Wstrict-null-sentinel -Wtrampolines -Wvector-operation-performance -Wno-terminate -Wno-aligned-new -Wno-switch-default)
