2a1875d32b02d464efa99fd05b2352961160d06c
Gary Furnish <gfurnish@gfurnish.net>
2017-10-15 01:26:36 +0000

    Add boost wrapper

---
+++ mcpputil/include/mcpputil/mcpputil/boost/log/trivial.hpp
@@ -0,0 +1,6 @@
+#pragma once
+// clang-format off
+#include <mcpputil/mcpputil/warning_wrapper_push.hpp>
+#include <boost/log/trivial.hpp>
+#include <mcpputil/mcpputil/warning_wrapper_pop.hpp>
+// clang-format on
